5c4faadd73c7b64572819af851e10dc7078d7399
yanivbyd <yanivby@artwaresoft.com>
2025-09-01 18:31:28 +0300

    Performance

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,10 +4,10 @@ set -e
 ./kill_all.sh
 rm -rf output/logs/*
 
-cargo run -p backend &
+cargo run --profile=balanced -p backend &
 sleep 1
 
-cargo run -p coordinator &
+cargo run --profile=balanced -p coordinator &
 sleep 1
 
-cargo run -p gui
+cargo run --profile=balanced -p gui
--- show_colony.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall "QuickTime Player"
-rm -rf output/colony_video.mp4
-#cargo run -p frontend -- --video && open output/colony_video.mp4
-cargo run -p frontend && open output/colony.png
